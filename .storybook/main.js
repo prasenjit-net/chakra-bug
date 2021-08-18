9f23262a7dd9edf33fa443e9308e85c8776c9465
Prasenjit Purohit <github@prasenjit.net>
2021-08-18 16:38:53 +0530

    workaround

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,5 +6,18 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@emotion/core": "@emotion/react",
+          "emotion-theming": "@emotion/react",
+        },
+      },
+    };
+  },
 }
